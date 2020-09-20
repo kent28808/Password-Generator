e00b7270f8cc3f5f05640d74a98834d345b634c7
kent28808 <kktanoue@gmail.com>
2020-09-19 23:37:35 -0700

    added functions to JS

--- Develop/script.js
+++ Develop/script.js
@@ -1,41 +1,60 @@
-// Variables
-  var special;
-  var numeric;
-  var lowercase;
-  var uppercase;
 
-  // Values
-  scharacters = ["!","@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "?", "<", ">", ":", "{", "}", "[", "]"];
-  ncharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-  lcharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-  ucharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-  // greater than 8 and less than 129
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+generateBtn.addEventListener("click", generatePassword);
   
+// Password Generator
  function generatePassword(){
-   var password = '';
-   for(var i = 0; i<l; i++){
-     password += characters.charAt(Math.floor(Math.random() * characters.length));
-   }
-   return password;
- } 
+ // Arrays
+  scharacters = ["!","@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "?", "<", ">", ":", "{", "}", "[", "]"];
+  ncharacters = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  lcharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  ucharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  // greater than 8 and less than 129
+
+   var userInput = prompt("How many characters would you like your password to contain? (08-128)");
+   userInput = parseInt(userInput)
+   if (userInput < 8 || userInput > 128 || isNaN(userInput)){
+    alert("Password length must be 08 and 128 characters.")
+    return;
+  } 
   
+  var store = [];
+  var upperInput = confirm("Click OK to confirm including uppercase characters"); 
+  if (upperInput){
+    store += store.concat(ucharacters) 
+  }
+  var lowerInput = confirm("Click OK to confirm including lowercase characters");
+  if (lowerInput){
+    store += store.concat(lcharacters)
+  } 
+  var specialInput = confirm("Click OK to confirm including specialcase characters");
+  if (specialInput){
+    store += store.concat(scharacters)
+  } 
+  var numericInput = confirm("Click OK to confirm including numericcase characters");
+  if (numericInput){
+    store += store.concat(ncharacters)
+  }  
 
-// link button to alerts
+  var fPassword = "";
 
-// alert 1-How many characters would you like your password to contain? (08-128)
-// If below 08 then alert says: "Password length must be at least 8 characters"
-// If above 128 then alert says: "Password length must be less than 129 characters"
 
-// if within range, series of prompts
-// prompt 1 "Click OK to confirm including special characters"
-// prompt 2 "Click OK to confirm including numeric characters"
-// prompt 3 "Click OK to confirm including lowercase characters"
-// prompt 4 "Click OK to confirm including uppercase characters"
 
-// If user selects cancel to all prompts, alert "Must select at least one charcter type"
+  //  Random selection
+  for (var i = 0; i < userInput; i++){
+    var random = Math.floor(Math.random() * store.length);
+    fPassword += store[random];
+  }console.log(fPassword)
+  var password = fPassword;
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+}
+   
 
-// Generated PW must then be written to page.
+// Write password to the #password input
+// function writePassword() {
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-  var characters = ("scharacters", "ncharacters", "lcharacters", "ucharacters")
+
+
+
